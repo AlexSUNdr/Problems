9e0ba064af48316c9c3fd80c1473813018391000
Alexander <49512505+AlexSUNdr@users.noreply.github.com>
2020-06-19 14:07:32 +0300

    Update solve-problem1.py

--- solve-problem1.py
+++ solve-problem1.py
@@ -1,5 +1,18 @@
 def compressed_vector(arr):
     new_arr = []
+	i = 1
+	leng = 1
+	while i < len(arr):
+		print(i)
+		if arr[i] == arr[i - 1]:
+			i = i + 1
+			leng = leng + 1
+		else:
+			new_arr.append((arr[i - 1], leng))
+			leng = 1
+			i = i + 1
+	new_arr.append((arr[i - 1], leng))
+	return new_arr
     
         
     
